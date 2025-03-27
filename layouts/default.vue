71686d2c23dc06c0ded3b383de8c0463d0a2aa95
kuskyst <126965999+kuskyst@users.noreply.github.com>
2025-03-27 19:36:03 +0900

    fitst

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app class="bg-secondary">
+    <v-app-bar color="transparent" flat class="position-relative pt-4 pb-4">
+
+      <v-app-bar-title>○○××</v-app-bar-title>
+
+      <template v-slot:append>
+        <v-app-bar-nav-icon v-if="!drawer" @click="drawer = !drawer" />
+        <v-btn v-else icon="mdi-close" @click="drawer = !drawer" />
+      </template>
+    </v-app-bar>
+    <v-navigation-drawer
+      temporary
+      location="right"
+      app
+      :mini-variant="!miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      class="bg-secondary fill-height top-0 right-0 position-fixed"
+    >
+      <v-list class="mt-16">
+        <v-list-item>
+          <v-btn variant="plain" width="100%" class="font-weight-bold">○○××</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn variant="plain" width="100%" class="font-weight-bold">○○××</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn variant="plain" width="100%" class="font-weight-bold">○○××</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn variant="plain" width="100%" class="font-weight-bold">○○××</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn variant="plain" width="100%" class="font-weight-bold">○○××</v-btn>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+const miniVariant = ref(false)
+const clipped = ref(true)
+const drawer = ref(false)
+</script>
---
+++ utils/vuetify.ts
@@ -0,0 +1,29 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import * as labsComponents from 'vuetify/labs/components'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+  components: {
+    ...components,
+    ...labsComponents
+  },
+  directives: { directives },
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#3E4B8F',
+          secondary: '#2A9DDE',
+          myorange: 'FC9B31',
+          mypink: 'FAC4D4',
+          mylightblue: 'AEDAF2',
+        }
+      }
+    }
+  }
+})
+
+export default vuetify
